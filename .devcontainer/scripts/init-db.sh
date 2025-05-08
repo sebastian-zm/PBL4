1fe3df2148f4e6f65c33f4bc35023bb646f2fce1
Sebastián Zevallos Márquez <email@nonexistent.invalid>
2025-05-08 09:12:14 +0000

    fix db

--- .devcontainer/scripts/init-db.sh
+++ .devcontainer/scripts/init-db.sh
@@ -8,10 +8,10 @@ for i in {1..30}; do
 done
 
 echo "=> Configurando usuarios y base de datos"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" << 'EOF'
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" << EOF
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS oposiciones CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS 'dev'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS 'dev'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON oposiciones.* TO 'dev'@'%';
 FLUSH PRIVILEGES;
 EOF
