4f54dc016fde58a83070f12b20b58800f67b1aa9
Sebastián Zevallos Márquez <email@nonexistent.invalid>
2025-05-08 15:10:22 +0000

    Make a working install of required dependencies.

---
+++ src/main/java/software/sebastian/oposiciones/OposicionesApplication.java
@@ -0,0 +1,13 @@
+package software.sebastian.oposiciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OposicionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OposicionesApplication.class, args);
+	}
+
+}
