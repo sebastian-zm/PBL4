7ff1e0f8118cd0c6468aa56fd05498ac1cdce2dc
Sebastian <sebastian@sebastian.software>
2025-05-29 15:00:11 +0200

    Eliminar innecesarios

--- .devcontainer/workspace_tmp/src/main/java/software/sebastian/oposiciones/repository/ConvocatoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package software.sebastian.oposiciones.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import software.sebastian.oposiciones.model.Convocatoria;
-
-public interface ConvocatoriaRepository extends JpaRepository<Convocatoria, Integer> {
-    // podrías añadir métodos como findByBoeId(...) si los necesitas
-}
